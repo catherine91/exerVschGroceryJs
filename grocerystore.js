c8203f9cc2c027b5ed2d74c27e5e336966682fd6
catherine91 <70351707+catherine91@users.noreply.github.com>
2020-11-16 22:09:09 -0700

    changes made by Catherine

---
+++ grocerystore.js
@@ -0,0 +1,34 @@
+var shopper = {
+    firstName = "Catherine"
+    lastName = "Turcotte"
+    age = 29
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+      }
+    };
+    
+import java.util.ArrayList;
+
+public class MyClass { 
+  public static void main(String[] args) { 
+    ArrayList<String> grocerycart = new ArrayList<String>();
+    grocerycart.add("Butter");
+    grocerycart.add("Chicken");
+    grocerycart.add("Eggs");
+    grocerycart.add("Carrots");
+    for (int i = 0; i < grocerycart.size(); i++) {
+      System.out.println(grocerycart.get(i));
+    }
+  } 
+}
+
+}
+
+public class CatherineAge {
+    public static void main(String[] args) {
+      boolean Catherineis29 = true;
+      boolean Catherineis28 = false;    
+      System.out.println(Catherineis29);
+      System.out.println(Catherineis28);
+    }
+  }
